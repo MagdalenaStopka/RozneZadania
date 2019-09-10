cfe91840edc5eea3c169d3d2975c17ae1daa492f
Magdalena Stopka <m.stopka@microsoft.wsei.edu.pl>
2019-09-10 16:00:37 +0200

    zadanie 5

---
+++ Zadanie5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wprowadz liczbę, którą chcesz spotęgować:");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Wprowadz wykładnik potęgi:");
+            string inputPower = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            int numberPower = Convert.ToInt32(inputPower);
+            CountPower(number, numberPower);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Wprowadz liczbę, z której chcesz obliczyc silnię:");
+            string inputFactorial = Console.ReadLine();
+            int numberFactorial = Convert.ToInt32(inputFactorial);
+            CountFactorial(numberFactorial);
+            Console.ReadKey();
+        }
+
+        public static void CountPower(int a, int b)
+        {
+            int poweredNumber = 1;
+            for (int i = 0; i < b; i++)
+            {
+                poweredNumber *= a;
+            }
+            Console.WriteLine("Wynik operacji: "+ poweredNumber);
+        }
+
+        public static void CountFactorial(int a)
+        {
+            int factorialNumber = 1;
+            for (int i = a; i > 0; i--)
+            {
+                factorialNumber *= i;
+            }
+            Console.WriteLine("Wynik operacji: " + factorialNumber);
+        }
+
+    }
+}
---
+++ Zadanie6/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadanie6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wprowadz liczbę:");
+            string input = Console.ReadLine();
+            CountZero(input);
+            PositionOfZero(input);
+
+            
+            Console.ReadKey();
+        }
+
+        public static void CountZero(string a)
+        {
+            int count = 0;
+
+            foreach (char number in a)
+            {
+                if (number == '0')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Liczba zer w wprowadzonej liczbie: "+ count);
+        }
+
+        public static void PositionOfZero(string a)
+        {
+            int position = 0;
+             int count = 0;
+            int index = 0;
+
+            Console.WriteLine("Pozycja zer w liczbie: ");
+
+            foreach (char number in a)
+            {
+                count++;
+                if (number == '0')
+                {
+                    index = a.NextIndexOf('0');
+                    position = index;
+                    if (index == a.LastIndexOf('0'))
+                    {
+                        Console.Write(position);
+                    }
+                    else
+                    {
+                        Console.Write(position.ToString() + ", ");
+                    }
+                }
+            }
+        }
+    }
+}
